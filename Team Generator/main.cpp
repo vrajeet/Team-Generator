9479a3a0054f1c513aead088bbb2f5f97bf26f86
vrajeet <vrajp7278@gmail.com>
2023-07-03 14:35:03 -0600

    Initial Commit

---
+++ Team Generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Team Generator
+//
+//  Created by Vraj Patel on 2023-07-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
